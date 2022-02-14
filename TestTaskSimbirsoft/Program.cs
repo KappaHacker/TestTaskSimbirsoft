a50ca92bf5bd5fe360193dde58daced0d0059f3f
KappaHacker <drkappa@mail.ru>
2022-02-14 23:21:19 +0500

    Fix

--- TestTaskSimbirsoft/Program.cs
+++ TestTaskSimbirsoft/Program.cs
@@ -4,38 +4,52 @@ using OpenQA.Selenium.Chrome;
 
 namespace TestTaskSimbirsoft
 {
-    class Program
+    class SearchGoogle
     {
-        static void Main(string[] args)
+        IWebDriver _driver;
+        By searchTextBox = By.XPath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input");
+        By searchButton = By.XPath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[3]/center/input[1]");
+        public SearchGoogle(IWebDriver driver)
         {
-            //Открытие вкладки с калькулятором Chrome
-            IWebDriver driver = new ChromeDriver();
-            driver.Navigate().GoToUrl(@"http://google.com");
-            IWebElement searchTextBox = driver.FindElement(By.XPath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input"));
-            IWebElement searchButton = driver.FindElement(By.XPath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[3]/center/input[1]"));
-            searchTextBox.SendKeys("калькулятор");
-            searchButton.Click();
-            //Получение кнопок 1*2-3+1=
-            IWebElement one = driver.FindElement(By.CssSelector("#rso > div:nth-child(1) > div > div > div.card-section > div > div > div.SKWP2e > div > table.ElumCf > tbody > tr:nth-child(4) > td:nth-child(1) > div > div"));
-            IWebElement mult = driver.FindElement(By.CssSelector("#rso > div:nth-child(1) > div > div > div.card-section > div > div > div.SKWP2e > div > table.ElumCf > tbody > tr:nth-child(3) > td:nth-child(4) > div > div"));
-            IWebElement two = driver.FindElement(By.CssSelector("#rso > div:nth-child(1) > div > div > div.card-section > div > div > div.SKWP2e > div > table.ElumCf > tbody > tr:nth-child(4) > td:nth-child(2) > div > div"));
-            IWebElement minuse = driver.FindElement(By.CssSelector("#rso > div:nth-child(1) > div > div > div.card-section > div > div > div.SKWP2e > div > table.ElumCf > tbody > tr:nth-child(4) > td:nth-child(4) > div > div"));
-            IWebElement three = driver.FindElement(By.CssSelector("#rso > div:nth-child(1) > div > div > div.card-section > div > div > div.SKWP2e > div > table.ElumCf > tbody > tr:nth-child(4) > td:nth-child(3) > div > div"));
-            IWebElement plus = driver.FindElement(By.CssSelector("#rso > div:nth-child(1) > div > div > div.card-section > div > div > div.SKWP2e > div > table.ElumCf > tbody > tr:nth-child(5) > td:nth-child(4) > div > div"));           
-            IWebElement button = driver.FindElement(By.CssSelector("#rso > div:nth-child(1) > div > div > div.card-section > div > div > div.SKWP2e > div > table.ElumCf > tbody > tr:nth-child(5) > td:nth-child(3) > div"));
-            //Нажатие кнопок
-            one.Click();
-            mult.Click();
-            two.Click();
-            minuse.Click();
-            three.Click();
-            plus.Click();
-            one.Click();
-            button.Click();
-            //Проверка результата
-            IWebElement history = driver.FindElement(By.CssSelector("#rso > div:nth-child(1) > div > div > div.card-section > div > div > div.BRpYC > div.TIGsTb > div.xwgN1d.XSNERd > div > span"));
-            IWebElement result = driver.FindElement(By.CssSelector("#rso > div:nth-child(1) > div > div > div.card-section > div > div > div.BRpYC > div.TIGsTb > div.fB3vD > div > div"));
-            if(history.Text == "1 × 2 - 3 + 1 =" && result.Text == "0")
+            _driver = driver;
+        }
+        public void OpenCalc()
+        {
+            _driver.Navigate().GoToUrl(@"http://google.com");
+            _driver.FindElement(searchTextBox).SendKeys("калькулятор");
+            _driver.FindElement(searchButton).Click();
+        }
+    }
+    class Calculator
+    {
+        IWebDriver _driver;
+        By one = By.CssSelector("#rso > div:nth-child(1) > div > div > div.card-section > div > div > div.SKWP2e > div > table.ElumCf > tbody > tr:nth-child(4) > td:nth-child(1) > div > div");
+        By mult = By.CssSelector("#rso > div:nth-child(1) > div > div > div.card-section > div > div > div.SKWP2e > div > table.ElumCf > tbody > tr:nth-child(3) > td:nth-child(4) > div > div");
+        By two = By.CssSelector("#rso > div:nth-child(1) > div > div > div.card-section > div > div > div.SKWP2e > div > table.ElumCf > tbody > tr:nth-child(4) > td:nth-child(2) > div > div");
+        By minuse = By.CssSelector("#rso > div:nth-child(1) > div > div > div.card-section > div > div > div.SKWP2e > div > table.ElumCf > tbody > tr:nth-child(4) > td:nth-child(4) > div > div");
+        By three = By.CssSelector("#rso > div:nth-child(1) > div > div > div.card-section > div > div > div.SKWP2e > div > table.ElumCf > tbody > tr:nth-child(4) > td:nth-child(3) > div > div");
+        By plus = By.CssSelector("#rso > div:nth-child(1) > div > div > div.card-section > div > div > div.SKWP2e > div > table.ElumCf > tbody > tr:nth-child(5) > td:nth-child(4) > div > div");
+        By button = By.CssSelector("#rso > div:nth-child(1) > div > div > div.card-section > div > div > div.SKWP2e > div > table.ElumCf > tbody > tr:nth-child(5) > td:nth-child(3) > div");
+        By history = By.CssSelector("#rso > div:nth-child(1) > div > div > div.card-section > div > div > div.BRpYC > div.TIGsTb > div.xwgN1d.XSNERd > div > span");
+        By result = By.CssSelector("#rso > div:nth-child(1) > div > div > div.card-section > div > div > div.BRpYC > div.TIGsTb > div.fB3vD > div > div");
+        public Calculator(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+        public void EnterTask()
+        {
+            _driver.FindElement(one).Click();
+            _driver.FindElement(mult).Click();
+            _driver.FindElement(two).Click();
+            _driver.FindElement(minuse).Click();
+            _driver.FindElement(three).Click();
+            _driver.FindElement(plus).Click();
+            _driver.FindElement(one).Click();
+            _driver.FindElement(button).Click();
+        }
+        public void CheckResukt()
+        {
+            if (_driver.FindElement(history).Text == "1 × 2 - 3 + 1 =" && _driver.FindElement(result).Text == "0")
             {
                 Console.BackgroundColor = ConsoleColor.Green;
                 Console.WriteLine("TEST PASSED");
@@ -46,11 +60,23 @@ namespace TestTaskSimbirsoft
                 Console.WriteLine("TEST FAILLED");
             }
             Console.ResetColor();
-            //Скриншот результата
-            Screenshot ss = ((ITakesScreenshot)driver).GetScreenshot();
+            Screenshot ss = ((ITakesScreenshot)_driver).GetScreenshot();
             ss.SaveAsFile(@"D://Image.png",
             ScreenshotImageFormat.Png);
-            //Закрытие вкладки
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            SearchGoogle page = new SearchGoogle(driver);
+            page.OpenCalc();
+            Calculator calc = new Calculator(driver);
+            calc.EnterTask();
+            calc.CheckResukt();
+
             driver.Quit();
         }
     }
